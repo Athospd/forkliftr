e352030b6673c175f155aef535225bfb85cd891a
ctlente <ctlente@gmail.com>
2017-07-03 15:04:48 +0000

    Cosmetic changes

--- R/guessers.R
+++ R/guessers.R
@@ -21,7 +21,7 @@ guess_delim <- function(file, n_max = 10, verbose = FALSE) {
   non_delimiter_characters <- purrr::map_chr(c(letters, LETTERS, 0:9, ".", "'", '"'), ~ .x %>% charToRaw %>% as.character)
   
   
-  # the candidates to be delims
+  # The candidates to be delims
   probable_delims <- lines %>%
     purrr::map_df(~count_chars(.x), .id = "line") %>%  # Get char count for each line
     dplyr::filter(!char_raw %in% non_delimiter_characters) %>% # disconsider letters and numbers for delimiter candidates
@@ -73,30 +73,28 @@ guess_has_header <- function(file, n_max = 10, verbose = FALSE) {
 
 # guess_comment
 
-# 
-
 # Detect and return a tabular file configuration
 frk_summarise_tabular_file <- function(file, n_max = 10) {
-  # guess delim
+  
+  # Guess delim
   guessed_delim = guess_delim(file)$char[1]
   
-  # guess encoding
+  # Guess encoding
   guessed_encoding = readr::guess_encoding(file)$encoding[1]
   
-  # guess has header
+  # Guess has header
   guess_has_header = guess_has_header(file)
   
   return(list(
     file = file,
     guessed_delim = guessed_delim,
     guessed_encoding = guessed_encoding,
-    guess_has_col_names = guess_has_col_names
+    guess_has_header = guess_has_header
   ))
 }
 
 # Detect and return all tabular files configurations from a directory
 frk_summarise_tabular_files <- function(path) {
   files <- list.files(path, full.names = TRUE)
-  
   summary <- purrr::map_df(files, frk_summarise_tabular_file)
 }
