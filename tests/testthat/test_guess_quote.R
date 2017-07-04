a09c1ab11c7cf233e4562c5f1a1a730acbb3d9d0
Athos Petri Damiani <600428@edp.com.br>
2017-07-04 17:05:13 -0300

    test for unquoted files + unquoted file treatment in guess_quote

--- tests/testthat/test_guess_quote.R
+++ tests/testthat/test_guess_quote.R
@@ -10,11 +10,18 @@ tiny <- tibble(
   b = c("as,df", "as,df", "gf,ds"))
 file_tiny1 <- tempfile()
 file_tiny2 <- tempfile()
+file_tiny_unquoted1 <- tempfile()
 write_csv(tiny, file_tiny1)
 write_delim(tiny, file_tiny2, ";")
+write.csv(tiny, file_tiny_unquoted1, quote = FALSE)
 
 test_that("guess_quote can guess from tiny tables", {
   expect_identical(guess_quote(file_tiny1), "\"")
   expect_true(is.na(guess_quote(file_tiny2)))
   expect_message(guess_quote(file_tiny, verbose = TRUE), "probable")
+})
+
+test_that("guess_quote can guess that there is no quote", {
+  expect_message(guess_quote(file_tiny_unquoted1, verbose = TRUE), "Most probable quote: '' (unquoted)")
+  expect_identical(guess_quote(file_tiny_unquoted1), "")
 })
