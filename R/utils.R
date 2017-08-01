cf4d11cce965f80689a5850603419660d3180c59
Athos Petri Damiani <600428@edp.com.br>
2017-08-01 19:07:33 -0300

    tide_names, enum_repeated_names and rm_accent included in utils.R + encoding as parameter of guess_col_types

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,102 @@ globalVariables(c(
   "a_priori_delimiter_ranks", "even",
   ".", "char", "comma", "period",
   "comma_g1", "period_g1", "header"))
+
+#' Tide up names to fit as column names of a data.frame
+#' 
+#' @description
+#' Sanitize a character vector to fit as column names.
+#' 
+#' @param x a character vector.
+#' 
+#' @examples
+#' 
+#' tide_names(names(iris))
+#' tide_names(c("repeated_name", "Repeated___NAME", "RePeated...nAMe  "))
+#' tide_names(c("snakeCase", "snake_case", "snake CasE", "snake Cas E"))
+#' 
+#' @export
+tide_names <- function(x) {
+  x %>%
+    # remove extra spaces from extremes
+    stringr::str_trim(side = "both") %>%
+    # all chars to lower case
+    base::tolower() %>%
+    # remove accents
+    rm_accent() %>%
+    # replace spaces, periods and slashes to _
+    stringr::str_replace_all("[ \\./\\\\]", "_") %>%
+    # replace spaces or sequence of _'s to a single _
+    stringr::str_replace_all(" |_{2,}", "_") %>%
+    # remove everything that is no alphanumeric nor _
+    stringr::str_replace_all("[^a-z0-9_]|_+$", "") %>%
+    # enumerate repeated names if any
+    enum_repeated_names()
+}
+
+
+#' Remove accents from words
+#' 
+#' @description
+#' Replace each character with it accentless version, including cedillas.
+#' 
+#' Current accents:
+#' 
+#' acute = áéíóúÁÉÍÓÚýÝ
+#' grave = àèìòùÀÈÌÒÙ
+#' circunflex = âêîôûÂÊÎÔÛ
+#' tilde = "ãõÃÕñÑ
+#' umlaut = äëïöüÄËÏÖÜÿ
+#' cedil = çÇ
+#' 
+#' @param str a character vector.
+#' 
+#' @examples
+#' 
+#' rm_accent(c("àquela", "Ñ", "opção", "açaí"))
+#' 
+#' @export
+rm_accent <- function(str) {
+  if(!is.character(str)) str <- as.character(str)
+  symbols <- c(
+    acute = "áéíóúÁÉÍÓÚýÝ",
+    grave = "àèìòùÀÈÌÒÙ",
+    circunflex = "âêîôûÂÊÎÔÛ",
+    tilde = "ãõÃÕñÑ",
+    umlaut = "äëïöüÄËÏÖÜÿ",
+    cedil = "çÇ"
+  )
+  nudeSymbols <- c(
+    acute = "aeiouAEIOUyY",
+    grave = "aeiouAEIOU",
+    circunflex = "aeiouAEIOU",
+    tilde = "aoAOnN",
+    umlaut = "aeiouAEIOUy",
+    cedil = "cC"
+  )
+  return(chartr(paste(symbols, collapse=""), paste(nudeSymbols, collapse=""), str))
+}
+
+#' Enumarate repeated names
+#' 
+#' @description
+#' Enumarate repeated names and put it as suffix to make every name from a vector unique.
+#' 
+#' @param str a character vector.
+#' 
+#' @seealso [tide_names()]
+#' 
+#' @examples
+#' 
+#' enum_repeated_names(c("a", "a", "b", "c", "c", "c", "d"))
+#' 
+#' @export
+enum_repeated_names <- function(str) {
+  data.frame(name = str) %>%
+    dplyr::group_by(name) %>%
+    dplyr::mutate(rank = 1:length(name),
+                  repeated = sum(rank) > 1,
+                  suffix = dplyr::if_else(repeated, as.character(rank), "")) %>%
+    tidyr::unite(name_suffix, name, suffix, sep = "") %>%
+    dplyr::pull(name_suffix)
+}
